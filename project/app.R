2611a058936a296c4df32449be15efd541cd520f
Felix Michalik <felix.michalik.16@aberdeen.ac.uk>
2019-05-12 00:05:00 +0200

    shiny outline

--- project/app.R
+++ project/app.R
@@ -8,41 +8,124 @@
 #jnjk
 
 library(shiny)
+library(leaflet)
+library(dplyr)
+library(leaflet.extras)
+library(shinydashboard)
+library(tmap)
+library(sf)   
+library(raster)
+library(spData)
+
+data <- read.csv("worldearthquakes.csv")
+#categorize earthquake depth
+
+data$depth_type <- ifelse(data$depth <= 70, "shallow", ifelse(data$depth <= 300 | data$depth >70, "intermediate", ifelse(data$depth > 300, "deep", "other")))
+
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+ui <- dashboardPage(skin = "blue",
+                    title = "Google Location Map",
+                    dashboardHeader(title = "Google Location Map", titleWidth = 300),
+                    
+                    # interactive sidebar with menu and widgets
+                    dashboardSidebar(width = 300,
+                                     
+                                     tags$div(
+                                       tags$blockquote("Use this app to see where Google has tracked you!"),
+                                       tags$p(checkboxInput("markers", "Depth", FALSE)),
+                                       tags$p(checkboxInput("heat", "Heatmap", FALSE)),
+                                       #tags$p(checkboxInput("country", "Switzerland", FALSE)),
+                                       textInput(inputId = "country", label = "Country", value = "Type country here", width = NULL,
+                                                 placeholder = NULL),
+                                       tags$h4("How to get your Google location data:"),
+                                       tags$p("Visit ", tags$a(href="https://takeout.google.com/", "Google Takeout")," to see and download any of the data Google holds on you."),
+                                       tags$p("Click on SELECT NONE, then scroll down to Location History and click on the slider to select it."),
+                                       tags$p("Scroll to the bottom and click NEXT, then CREATE ARCHIVE, and finally DOWNLOAD when it is ready. You will need to verify by logging into your Google account."),
+                                       tags$p("This will download a ZIP file to your downloads directory. Extract this ZIP file which will create a directory called Takeout"),
+                                       tags$p("Upload the JSON file found in Takeout/Location History using the selector below..."),
+                                       style = "padding: 10px;"
+                                       
+                                     )
+                                     
+                                     
+                                     
+                                     
+                                     
+                    ),
+                    
+                    # Main panel for displaying outputs ----
+                    dashboardBody(
+                      
+                      tags$head(tags$style("#mymap{height:90vh !important;}")),
+                      
+                      leafletOutput(outputId = "mymap")
+                      
+                      
+                      
+                    )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+server <- function(input, output, session) {
+  
+  #define the color pallate for the magnitidue of the earthquake
+  pal <- colorNumeric(
+    palette = c('gold', 'orange', 'dark orange', 'orange red', 'red', 'dark red'),
+    domain = data$mag)
+  
+  
+  #define the color of for the depth of the earquakes
+  pal2 <- colorFactor(
+    palette = c('blue', 'yellow', 'red'),
+    domain = data$depth_type
+  )
+  
+  
+  #create the map
+  output$mymap <- renderLeaflet({
+    leaflet(data) %>% 
+      addProviderTiles(providers$CartoDB.Positron, group = "Default Maptile") %>% 
+      addProviderTiles(providers$CartoDB.DarkMatter, group = "Dark Maptile") %>%
+      addProviderTiles(providers$Esri.WorldImagery, group = "Satellite Maptile") %>%
+      setView(24, 27, zoom = 2) %>% 
+      addPolygons(data = world[world$name_long == input$country, ], fill = TRUE) %>%
+      #plot(world["pop"])   %>%
+      addLayersControl(
+        baseGroups = c("Default Maptile", "Dark Maptile", "Satellite Maptile"),
+        options = layersControlOptions(collapsed = FALSE)
+      )
+    
+  })
+  
+  #next we use the observe function to make the checkboxes dynamic. If you leave this part out you will see that the checkboxes, when clicked on the first time, display our filters...But if you then uncheck them they stay on. So we need to tell the server to update the map when the checkboxes are unchecked.
+  
+  observe({
+    proxy <- leafletProxy("mymap", data = data)
+    proxy %>% clearMarkers()
+    if (input$markers) {
+      proxy %>% addCircleMarkers(stroke = FALSE, color = ~pal2(depth_type), fillOpacity = 0.2,      label = ~as.character(paste0("Magnitude: ", sep = " ", mag))) %>%
+        addLegend("bottomright", pal = pal2, values = data$depth_type,
+                  title = "Depth Type",
+                  opacity = 1)}
+    else {
+      proxy %>% clearMarkers() %>% clearControls()
+    }
+  })
+  
+  observe({
+    proxy <- leafletProxy("mymap", data = data)
+    proxy %>% clearMarkers()
+    if (input$heat) {
+      proxy %>%  addHeatmap(lng=~longitude, lat=~latitude, intensity = ~mag, blur =  10, max = 0.05, radius = 15) 
+    }
+    else{
+      proxy %>% clearHeatmap()
+    }
+    
+    
+  })
+  
 }
 
 # Run the application 
